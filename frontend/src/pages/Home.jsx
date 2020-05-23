1d2289831a9f45d594e5e8a27380580240de0fda
YotamShinan <yshinan@gmail.com>
2020-05-23 11:33:58 +0300

    after adding search bar, and city preview

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,30 +2,18 @@ import { loadCities } from "../actions/cityActions";
 import { connect } from "react-redux";
 import React, { Component } from "react";
 import CityList from "../cmps/CityList";
+import SearchBar from '../cmps/SearchBar';
 
 class Home extends Component {
   componentDidMount() {
-    console.log("comp");
     this.props.loadCities();
   }
 
   render() {
-    console.log("cities", this.props.cities);
-
     return (
       <div className="home-page-container">
-        <header>
-          <div>LOGO HERE</div>
-          <div>NAVBAR HERE</div>
-        </header>
         <div className="hero hero-home-page">
-          <img
-            className="home-page-img"
-            src="https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-          />
-          <h1>Welcome To Our Amazing Food Lovers App</h1>
-          <input placeholder="Search Bar" />
-          <button>Search</button>
+          <SearchBar />
         </div>
         <section className="cities-container">
           <ul>
@@ -33,10 +21,7 @@ class Home extends Component {
           </ul>
         </section>
         <section className="products-container">
-            <ul>
-
-            </ul>
-
+          <ul></ul>
         </section>
       </div>
     );
