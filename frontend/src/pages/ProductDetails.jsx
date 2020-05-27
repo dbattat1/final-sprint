79b0abcf9c6b6b67f4e67c97d342a8fef9a0de3f
YotamShinan <yshinan@gmail.com>
2020-05-27 13:15:49 +0300

    wow

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,12 +1,12 @@
-import { loadProduct, updateProduct } from '../actions/productActions';
-import { addOrder,updateOrder } from '../actions/orderActions';
-import React from 'react';
-import { connect } from 'react-redux';
-import OrderForm from '../cmps/OrderForm.jsx';
-import ProductReview from '../cmps/ProductReview.jsx';
-import { Link } from 'react-router-dom';
+import { loadProduct, updateProduct } from "../actions/productActions";
+import { addOrder, updateOrder } from "../actions/orderActions";
+import React from "react";
+import { connect } from "react-redux";
+import OrderForm from "../cmps/OrderForm.jsx";
+import ProductReview from "../cmps/ProductReview.jsx";
+import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
-import  productService  from '../services/productService.js';
+import productService from "../services/productService.js";
 
 class ProductDetails extends React.Component {
   componentDidMount() {
@@ -20,66 +20,81 @@ class ProductDetails extends React.Component {
     const { id } = this.props.match.params;
     if (!id) return;
 
-    productService.get(id).then(user => {
-      const editedUser = { ...user }
+    productService.get(id).then((user) => {
+      const editedUser = { ...user };
       const cityId = editedUser.product.city._id;
       editedUser.product = null;
       this.props.updateProduct(editedUser);
-      this.props.history.push(`/${cityId}`); 
-    })
-  }
+      this.props.history.push(`/${cityId}`);
+    });
+  };
 
   render() {
-    const { user } = this.props
-    if (!user) return 'Loading...';
-    const {product} = user;
-    
-    return <div className="">
-      <div className="product-header flex column justify-center align-center">
-        <div className="title flex row">
-          <h1>{product.title}</h1>
-          <h3>{product.city.name}</h3>
-          <div style={{ zIndex: 100 }}>
-            <Link to={`/edit/${this.props.match.params.id}`}><button>✎</button></Link>
-            <button onClick={this.onRemoveProduct}><i className="trash icon"></i></button>
+    const { user } = this.props;
+    if (!user) return "Loading...";
+    const { product } = user;
+
+    return (
+      <div className="product-page">
+        <section className="product-page-header">
+          <div className="product-page-title">
+            <div>{product.title}</div>
+            <div>★8.2/10</div>
+            <div>{product.city.name}</div>
           </div>
+          <div className="product-page-edit">
+            <Link to={`/edit/${this.props.match.params.id}`}>
+              <button>✎</button>
+            </Link>
+            <button onClick={this.onRemoveProduct}>
+              <i className="trash icon"></i>
+            </button>
+          </div>
+        </section>
+
+        <div className="product-page-img-container">
+          <img alt="" className="prime-photo" src={`${product.imgUrls[0]}`} />
+          <img alt="" src={`${product.imgUrls[1]}`} />
+          <img alt="" src={`${product.imgUrls[2]}`} />
+          <img alt="" src={`${product.imgUrls[3]}`} />
+          <img alt="" src={`${product.imgUrls[4]}`} />
         </div>
-        <h4>★8.2/10</h4>
-      </div>
-      <div className="product-img-container">
-        <img alt="" className="prime-photo" src={`${product.imgUrls[0]}`} />
-        <img alt="" src={`${product.imgUrls[1]}`} />
-        <img alt="" src={`${product.imgUrls[2]}`} />
-        <img alt="" src={`${product.imgUrls[3]}`} />
-        <img alt="" src={`${product.imgUrls[4]}`} />
-      </div>
-      <div className="product-main-container flex space-around">
-        <div className="content flex column">
-          <h2>Lorem ipsum dolor sit amet, consectetur</h2>
-          <p>{product.description}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <h2>price per person: ${product.price}</h2>
+        <main className="product-page-main">
+          <div className="product-page-content">
+            <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+            <p>
+              {product.description}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat
+            </p>
+          </div>
+          <div className="order-form">
+            <OrderForm seller={user} />
+          </div>
+        </main>
+        <section className="product-page-reviews">
           <ProductReview id={this.props.match.params.id} />
-        </div>
-        <OrderForm seller={user} />
+        </section>
       </div>
-    </div>;
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    user: state.product.currProduct
+    user: state.product.currProduct,
   };
 };
-
 
 const mapDispatchToProps = {
   loadProduct,
   updateProduct,
   addOrder,
-  updateOrder
-}
+  updateOrder,
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductDetails));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(ProductDetails)
+);
