2ca4ecf89507b09cb2052f1ba2ee254c9b82cae3
dbattat1 <dbattat1@gmail.com>
2020-05-28 19:55:37 +0300

    shavuot3

--- backend/api/user/user.routes.js
+++ backend/api/user/user.routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth.middleware')
-const {getUser, getUsers, deleteUser, updateUser, getUsersByCity, getFavUsers} = require('./user.controller')
+const {getUser, getUsers, deleteUser, updateUser, getFavUsers} = require('./user.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
-router.get('/city/:id', getUsersByCity)
+// router.get('/city/:id', getUsersByCity)
 router.get('/fav/', getFavUsers)
 router.get('/', getUsers)
 router.get('/:id', getUser)
